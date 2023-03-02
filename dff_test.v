c0f1e0602fab7c3c98a6569d26042c435e15f529
khaja7289 <122887258+khaja7289@users.noreply.github.com>
2023-03-02 13:15:53 +0530

    Update dff_test.v

--- dff_test.v
+++ dff_test.v
@@ -1,1 +1,15 @@
+module dff_test(
+    output logic q,
+    input logic clk, d
+);
+dff uut(
+  .q(q),
+  .d(d),
+  .clk(clk)
+);
 
+initial begin
+  $dumpfile("dff.vcd");
+	$dumpvars;
+end
+endmodule
